e45b74849a3c1eabf9cc8482a0c01c6decbe56d2
Juan Carlos Mamani <juanqui725@gmail.com>
2026-04-22 18:02:05 -0400

    api terminado sin verificar

---
+++ api/src/Config/conexionDB.php
@@ -0,0 +1,14 @@
+<?php
+class ConexionPDO
+{
+    static private $cnn;
+    public function __construct() {
+        $pdo='mysql:host='.HOST.'port='.PORT.';dbname='.DATABASE.';charset='.CHARSET;
+        $options=[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>false];
+        try{
+            self::$cnn=new PDO($pdo, USER,PASSWORD,$options);
+            }
+            }catch(\PDOExeption $error)
+            {
+                die("ERROR ".$error->getMessage());
+            }
