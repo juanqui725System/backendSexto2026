57d275e9ffba2960b7365e93ba248bdba4eb9e61
Juan Carlos Mamani <juanqui725@gmail.com>
2026-04-29 15:14:39 -0400

    modificador 1 29-04-26

--- api/src/Config/conexionDB.php
+++ api/src/Config/conexionDB.php
@@ -6,9 +6,10 @@ class ConexionPDO
         $pdo='mysql:host='.HOST.'port='.PORT.';dbname='.DATABASE.';charset='.CHARSET;
         $options=[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>false];
         try{
-            self::$cnn=new PDO($pdo, USER,PASSWORD,$options);
-            }
-            }catch(\PDOExeption $error)
+            self::$cnn=new PDO($pdo,USERNAME,PASSWORD,$options);
+            }catch(PDOException $error)
             {
                 die("ERROR ".$error->getMessage());
             }
+    }
+}
